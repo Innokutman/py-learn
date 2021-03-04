bd8ddb6f3243d43f22cbac8ad496caf179cc6f61
[KeshaG] <[3768255@gmail.com]>
2021-03-04 21:58:26 +0500

    Solution for matrixElementsSum

--- matrixElementsSum.py
+++ matrixElementsSum.py
@@ -1,2 +1,13 @@
 # https://app.codesignal.com/arcade/intro/level-2/xskq4ZxLyqQMCLshr
 def matrixElementsSum(matrix):
+    sum = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if (matrix[i-1][j] == 0) and (i != 0):
+                matrix[i][j] = 0
+            sum += matrix[i][j]
+    return sum
+print(matrixElementsSum([
+[0,1,1,2], 
+[0,5,0,0], 
+[2,0,3,3]]))        
